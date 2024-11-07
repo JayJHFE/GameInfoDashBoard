707169f3c670ad3347eb680ed81d9aefd40b4f06
jhlee <Jaylee@groundk.com>
2024-11-07 17:04:49 +0900

    chore: 로테이션 챔피언 위치설정 및 이미지 추가

---
+++ src/app/components/character_li/character_li.tsx
@@ -0,0 +1,34 @@
+import character_li from './character_li.module.css'
+
+interface CharacterLiProps {
+
+    key: string;
+
+    champion: {
+
+      name: string;
+
+      tags: string;
+
+      image: string;
+
+      key: string;
+
+      id: string;
+
+    };
+
+  }
+
+export default function CharacterLi(data: CharacterLiProps) {
+    // console.log(data.champion.id);
+    // console.log(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`);
+    return <>
+            <li className={character_li.li}>
+                {/* <div> */}
+                    {/* <img src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`} alt={data.champion.name} /> */}
+                    <h3>{data.champion.name}</h3>
+                    <p>{data.champion.tags}</p>
+            </li>
+           </>
+}
--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -1,66 +1,15 @@
-// "use client";
-// import { useEffect, useState } from "react";
-
-// interface Champion {
-//   id: string;
-//   key: string;
-//   name: string;
-// }
-
-// interface ChampionData {
-//   [key: string]: Champion;
-// }
-
-// export default function MainClient() {
-//   const [data, setData] = useState(null);
-//   const [freeChampions, setFreeChampions] = useState([]);
-
-//   useEffect(() => {
-//     async function RotationChamp() {
-//       try {
-//         const response = await fetch("/api/loadRotationChamp");
-//         const result = await response.json();
-//         setData(result); // 상태에 저장
-
-//         const champResponse = await fetch("/LeagueofLegendData/champion.json");
-//         const championData = await champResponse.json();
-
-//         const matchedChampions = result.freeChampionIds.map(id => {
-//           const matchedChampion = Object.values(championData.data).find(
-//             (champ: Champion) => champ.key === id.toString()
-//           );
-//           return matchedChampion ? matchedChampion.name : null;
-//         }).filter(Boolean); // null 값 제거
-
-//         setFreeChampions(matchedChampions);
-//       } catch (error) {
-//         console.error("Error fetching data:", error);
-//       }
-//     }
-
-
-//     RotationChamp();
-//   }, []);
-
-//   return (
-//     <div>
-//       <h1>Data from API Route:</h1>
-//       <pre>{JSON.stringify(data, null, 2)}</pre>
-//     </div>
-//   );
-// }
-
-
 "use client";
-import { useEffect, useState } from "react";
 
-// 챔피언 데이터 타입 정의
+import { useEffect, useState } from "react";
+import CharacterLi from "../components/character_li/character_li";
+import characterStyles from './character_ul.module.css';
+
 interface Champion {
-  version: string;
+  tags: string;
   id: string;
   key: string;
   name: string;
-  title: string;
+  image: string;
 }
 
 interface ChampionData {
@@ -69,38 +18,39 @@ interface ChampionData {
 
 export default function MainClient() {
   const [data, setData] = useState<{ freeChampionIds: number[] } | null>(null);
-  const [freeChampions, setFreeChampions] = useState<string[]>([]);
+  const [freeChampions, setFreeChampions] = useState<
+    { name: string; tags: string; image: string; key: string; id: string; }[]
+  >([]);
 
   useEffect(() => {
     async function fetchData() {
       try {
-        // API 호출하여 로테이션 챔피언 ID 가져오기
         const response = await fetch("/api/loadRotationChamp");
         const result = await response.json();
         setData(result);
 
-        // json 파일 호출 후 챔피언 데이터 가져오기
         const champResponse = await fetch("/LeagueofLegendData/champion.json");
         const championDatajson: { data: ChampionData } = await champResponse.json();
         const championData = championDatajson.data;
+        console.log(championData);
 
-        // 로테이션 챔피언값과 챔피언 데이터를 비교하여 매칭된 챔피언 이름 배열 생성
         const matchedChampions = result.freeChampionIds.map((id: number) => {
           const matchedChampion = Object.values(championData).find((champ: Champion) => {
             return champ.key === id.toString();
           });
 
-          if (!matchedChampion) {
-            console.log(`Id값에 맞는 챔피언이 없습니다: ${id}`);
-          } else {
-            console.log(`챔피언을 찾았습니다: ${matchedChampion.name}, 아이디: ${id}`);
-          }
-
-          return matchedChampion ? matchedChampion.name : null;
-        });
-        setFreeChampions(matchedChampions); // 매칭된 챔피언 이름 배열을 상태에 저장
-
-
+          return matchedChampion
+            ? {
+                name: matchedChampion.name,
+                tags: matchedChampion.tags,
+                image: matchedChampion.image,
+                key: matchedChampion.key,
+                id: matchedChampion.id,
+              }
+            : null;
+        }).filter((champion : object) => champion !== null);
+        console.log(matchedChampions);
+        setFreeChampions(matchedChampions);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -111,14 +61,18 @@ export default function MainClient() {
 
   return (
     <div>
-      <h1>Free Champion Rotation:</h1>
-      {data && <pre>{freeChampions}</pre>}
-      {/* <h2>Free Champions by Name:</h2> */}
-      {/* <ul>
-        {freeChampions.map((name, index) => (
-          <li key={index}>{name}</li>
+      <h1>이번주 무료 챔피언</h1>
+      <ul className={characterStyles.ul_container}>
+        {freeChampions.map((freechampion) => (
+          <CharacterLi key={freechampion.key} champion={freechampion}/>
+          // <li key={index}>
+          //   <h3>{champion.name}</h3>
+          //   {/* <p>Tags: {champion.tags}</p>
+          //   <img src={champion.image} alt={`${champion.name} 이미지`} />
+          //   <p>Key: {champion.key}</p> */}
+          // </li>
         ))}
-      </ul> */}
+      </ul>
     </div>
   );
 }
