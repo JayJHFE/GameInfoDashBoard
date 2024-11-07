6dc2cba4118d3ab3a0cc4ed5d5416f200f22bb98
JayJHFE <dove92lee@gmail.com>
2024-11-07 20:32:19 +0900

    fix:오늘의 챔피언 UI수정

--- src/app/components/character_li/character_li.tsx
+++ src/app/components/character_li/character_li.tsx
@@ -1,34 +1,36 @@
-import character_li from './character_li.module.css'
+import character_li from "./character_li.module.css";
 
 interface CharacterLiProps {
+  key: string;
+
+  champion: {
+    name: string;
+
+    tags: string;
+
+    image: string;
 
     key: string;
 
-    champion: {
-
-      name: string;
-
-      tags: string;
-
-      image: string;
-
-      key: string;
-
-      id: string;
-
-    };
-
-  }
+    id: string;
+  };
+}
 
 export default function CharacterLi(data: CharacterLiProps) {
-    // console.log(data.champion.id);
-    // console.log(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`);
-    return <>
-            <li className={character_li.li}>
-                {/* <div> */}
-                    {/* <img src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`} alt={data.champion.name} /> */}
-                    <h3 className={character_li.h3}>{data.champion.name}</h3>
-                    <p>{data.champion.tags}</p>
-            </li>
-           </>
+  // console.log(data.champion.id);
+  // console.log(`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`);
+  return (
+    <>
+      <li className={character_li.li}>
+        {/* <div> */}
+        {/* <img
+          src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`}
+          alt={data.champion.name}
+          style={{ width: "100px", height: "20vh" }}
+        /> */}
+        <h3 className={character_li.h3}>{data.champion.name}</h3>
+        <p>{data.champion.tags}</p>
+      </li>
+    </>
+  );
 }
