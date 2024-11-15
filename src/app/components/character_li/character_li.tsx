e9b4c94cf03915de1d2ea66adaa0583bb04cd3fd
JayJHFE <dove92lee@gmail.com>
2024-11-15 21:30:26 +0900

    chore: css수정

--- src/app/components/character_li/character_li.tsx
+++ src/app/components/character_li/character_li.tsx
@@ -23,11 +23,11 @@ export default function CharacterLi(data: CharacterLiProps) {
     <>
       <li className={character_li.li}>
         {/* <div> */}
-        {/* <img
+        <img
           src={`https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${data.champion.id}_0.jpg`}
           alt={data.champion.name}
           style={{ width: "100px", height: "20vh" }}
-        /> */}
+        />
         <h3 className={character_li.h3}>{data.champion.name}</h3>
         <p>{data.champion.tags}</p>
       </li>
