843fa2e9f4f6257fb9e4c86e7178349e93a03048
JayJHFE <dove92lee@gmail.com>
2024-11-20 15:06:07 +0900

    chore: 롤 카드css 추가

--- src/app/components/randomChampionCard/randomChampionCard.tsx
+++ src/app/components/randomChampionCard/randomChampionCard.tsx
@@ -1,19 +1,23 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import cardStyles from './randomChampionCard.module.css';
 
 export default function RandomChampionCard() {
-    const cardRef = useRef<HTMLDivElement>(null);
+    const [isFlipped, setIsFlipped] = useState(false);
+    // const cardRef = useRef<HTMLDivElement>(null);
+    // const handleCardClick = () => {
+    //     if (cardRef.current) {
+    //         cardRef.current.classList.toggle(cardStyles.is_flipped);
+    //     }
+    // };
     const handleCardClick = () => {
-        if (cardRef.current) {
-            cardRef.current.classList.toggle(cardStyles.is_flipped);
-        }
+        setIsFlipped(!isFlipped);
     };
     return (
         <>
-            <div className={`${cardStyles.scene} ${cardStyles.scene_card}`} onClick={handleCardClick}>
-                <div ref={cardRef}className={cardStyles.card}>
-                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_front}`}>front</div>
-                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_back}`}>back</div>
+            <div className={`${cardStyles.scene} ${cardStyles.scene_card}`}>
+                <div className={`${cardStyles.card} ${isFlipped ? cardStyles.is_flipped : ''}`} onClick={handleCardClick}>
+                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_front}`}></div>
+                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_back}`}></div>
                 </div>
             </div>
         </>
