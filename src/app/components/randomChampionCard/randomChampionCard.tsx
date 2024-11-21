9367039528a0788299269f7b0a37c39d431b8a43
JayJHFE <dove92lee@gmail.com>
2024-11-21 14:08:16 +0900

    fix: css 수정

--- src/app/components/randomChampionCard/randomChampionCard.tsx
+++ src/app/components/randomChampionCard/randomChampionCard.tsx
@@ -23,8 +23,10 @@ interface AramChampionTierProps {
 export default function RandomChampionCard(allChampionData: AramChampionTierProps) {
     const [isFlipped, setIsFlipped] = useState(false);
     const [randomChampion, setRandomChampion] = useState<Champion | null>(null);
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
     const handleCardClick = async () => {
         if (!isFlipped) {
+            setImageUrl(null);
             const tmpRandomChampion =
                 allChampionData.allChampionData[
                     Object.keys(allChampionData.allChampionData)[
@@ -32,6 +34,13 @@ export default function RandomChampionCard(allChampionData: AramChampionTierProp
                     ]
                 ];
             setRandomChampion(tmpRandomChampion);
+            const image = new Image();
+            image.src = `https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${tmpRandomChampion.id}_0.jpg`;
+            image.onload = () => {
+                setImageUrl(image.src); // 이미지 URL 상태 업데이트
+                setIsFlipped(true); // 카드 플립
+            };
+
         }
         await new Promise((resolve) => {
             setIsFlipped(!isFlipped);
@@ -48,7 +57,8 @@ export default function RandomChampionCard(allChampionData: AramChampionTierProp
                 <div className={`${cardStyles.card} ${isFlipped ? cardStyles.is_flipped : ''}`} onClick={handleCardClick}>
                     <div className={`${cardStyles.card_face} ${cardStyles.card_face_front}`}></div>
                     {/* <img className={`${cardStyles.card_face} ${cardStyles.card_face_back}`} style={{backgroundImage: `url("https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${randomChampion?.id}_0.jpg")`, width:"100%", height:"100%", objectFit:"contain"}}></img> */}
-                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_back}`} style={{backgroundImage: `url("https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${randomChampion?.id}_0.jpg")`, backgroundSize: "contain", backgroundPosition:"center", backgroundRepeat:"no-repeat"}}>
+                    {/* <div className={`${cardStyles.card_face} ${cardStyles.card_face_back}`} style={{backgroundImage: `url("https://ddragon.leagueoflegends.com/cdn/img/champion/loading/${randomChampion?.id}_0.jpg")`, backgroundSize: "contain", backgroundPosition:"center", backgroundRepeat:"no-repeat"}}> */}
+                    <div className={`${cardStyles.card_face} ${cardStyles.card_face_back}`} style={{backgroundImage: imageUrl ? `url(${imageUrl})` : 'none', backgroundSize: "contain", backgroundPosition:"center", backgroundRepeat:"no-repeat"}}>
                         <p style={{position:"absolute", top: "46%", left:"5%", fontSize:"13px"}}>{randomChampion?.name}</p>
                     </div>
                 </div>
