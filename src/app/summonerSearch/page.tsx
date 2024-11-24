7941a162e3f1f312485f3001e8abd4dd73c4362c
JayJHFE <dove92lee@gmail.com>
2024-11-24 17:15:10 +0900

    chore: 소환사 전적검색 기능추가 중

--- src/app/summonerSearch/page.tsx
+++ src/app/summonerSearch/page.tsx
@@ -2,6 +2,10 @@ export default function SummonerSearch() {
   return (
     <div>
       <h1>Summoner Search</h1>
+      <div>
+        <h2>아이디를 입력해주세요</h2>
+        <input type="text" />
+      </div>
     </div>
   );
 }
