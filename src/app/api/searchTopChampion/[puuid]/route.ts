4e8d31c3c304c49dd93bddae1cd61df46d761e61
JayJHFE <dove92lee@gmail.com>
2024-11-26 22:13:05 +0900

    chore: 소환사 숙련도 조회 추가

---
+++ src/app/api/searchTopChampion/[puuid]/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: Request,
+  { params }: { params: { puuid: string } }
+) {
+  const { puuid } = params;
+
+  console.log("API Route hit");
+  console.log("Received PUUID:", puuid);
+
+  if (!puuid) {
+    return NextResponse.json({ error: "PUUID is missing" }, { status: 400 });
+  }
+
+  const url = `https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}/top?count=3`;
+  console.log("오찌도찌:", url);
+
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        "X-Riot-Token": "RGAPI-d0ef7619-7f3f-4fae-a38b-68eff7631dff",
+      },
+    });
+
+    if (!response.ok) {
+      const errorMessage = await response.text();
+      console.error(`Riot API Error: ${response.status} - ${errorMessage}`);
+      return NextResponse.json(
+        {
+          error: `Riot API responded with ${response.status}: ${errorMessage}`,
+        },
+        { status: response.status }
+      );
+    }
+
+    const data = await response.json();
+    return NextResponse.json(data);
+  } catch (error) {
+    console.error("Unhandled Error:", error);
+    return NextResponse.json(
+      { error: (error as Error).message || "Unknown error occurred" },
+      { status: 500 }
+    );
+  }
+}
