91a9c5a104263bc5b7f2cfa8f40ea4b6e501a506
JayJHFE <dove92lee@gmail.com>
2024-12-14 22:57:49 +0900

    chore: 최근게임 데이터 추가중

--- src/app/components/matchedGame/matchedGame.tsx
+++ src/app/components/matchedGame/matchedGame.tsx
@@ -1,25 +1,27 @@
 interface Participant {
-    [key: string]: string | number | boolean | object;
-  }
+  [key: string]: string | number | boolean | object;
+}
 interface InfoData {
-    endOfGameResult: string;
-    gameMode: string;
-    gameDuration: number;
-    gameId: number;
-    participants: Participant[];
+  endOfGameResult: string;
+  gameMode: string;
+  gameDuration: number;
+  gameId: number;
+  participants: Participant[];
 }
 interface GameData {
-    info : InfoData;
-    metatdata : {[key: string]: string | number | object;};
+  info: InfoData;
+  metatdata: { [key: string]: string | number | object };
 }
 interface MatchedGameProps {
-    gameData: GameData;
+  gameData: GameData;
 }
 
-export default function MatchedGame(gameData : MatchedGameProps) {
-    return (
-        <div>
-            <h1>Matched Game</h1>
-        </div>
-    );
+export default function MatchedGame(gameData: MatchedGameProps) {
+  return (
+    <div>
+      {gameData.info.participants.map((participant) => {})}
+      <div></div>
+      <h1>Matched Game</h1>
+    </div>
+  );
 }
