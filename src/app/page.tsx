0a6ec0ababe3b83b3f38eff1a9f078820d5f3ae9
JayJHFE <dove92lee@gmail.com>
2025-02-19 20:12:16 +0900

    chore: css  추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,11 @@ import Main from "./main/page"; // 클라이언트 컴포넌트 사용
 export default async function HomePage() {
   return (
     <div>
-      {/* <Main /> */}
+      <img
+        src="/summonerValley.jpg"
+        alt="summonerValley"
+        style={{ width: "100%", height: "100%" }}
+      />
     </div>
   );
 }
